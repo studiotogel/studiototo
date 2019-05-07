28f5d4dc85e605b2da5021b5902ceb2e83824450
studiotogel <50381064+studiotogel@users.noreply.github.com>
2019-05-08 02:46:03 +0700

    studiototo

---
+++ bandartoto.js
@@ -0,0 +1,81 @@
+document.write('<a href="http://linktoto.info/daftar24" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-Y5Jpd0ipcwc/XNHYBiKTzkI/AAAAAAAABrg/B3AkEEK6J8A_gFopshNWlqFf4w2IqdoswCLcBGAs/s1600/8Togel.png" alt="8TOGEL" title="8TOGEL" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar31" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-zgcJpPD7pgY/XNHYEayh2VI/AAAAAAAABsA/Gx0cdRuHRBADv1c9cza3TN7uP97vrv9fwCLcBGAs/s1600/afatogel.png" alt="AFATOGEL" title="AFATOGEL" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/agen4D" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-tR5fqv3CeJk/XNHYE1cC5OI/AAAAAAAABsE/KWU5bkrQ2A8yyXK9bo7K8oM205tdiCUrQCLcBGAs/s1600/agen4d.png" alt="AGEN4D" title="AGEN4D" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/anekatoto" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-k-Bv0R8d6lg/XNHYFVQt0nI/AAAAAAAABsI/ujnzeltRyG4Bqgd_D4uMLP0_h9__HflVQCLcBGAs/s1600/anekatoto.png" alt="ANEKATOTO" title="ANEKATOTO" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar6" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-LJu_igiC2CM/XNHYFYFSxCI/AAAAAAAABsM/Hs957BZrXMYvcT0zFAIAAAyyGKVedccyQCLcBGAs/s1600/angkajitutoto.png" alt="Angkajitutoto" title="angkajitutoto" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar20" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-OE31VgII_zY/XNHYBtTc_3I/AAAAAAAABrY/YHN2T91vH-Qr3bORtark3bSN6HcSG2wQQCLcBGAs/s1600/Alpha4D.png" alt="Alpha4D" title="Alpha4D" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar5" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-Mgiuhoqh6bY/XNHYFjNFm-I/AAAAAAAABsQ/LvjwE3nmjygKaFyR3bKEf66L2cQb8Z2XACLcBGAs/s1600/artistoto.png" alt="Artistoto" title="Artistoto" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar2" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-pfZo6g8Xrko/XNHYGG1BFnI/AAAAAAAABsU/_cHrqBYXGZM1BO7z5bf_bIbfcKBkpYJfQCLcBGAs/s1600/barcatoto.png" alt="Barcatoto" title="Barcatoto" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/bintang168" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-UZ8yHcY8SI0/XNHYGeKyorI/AAAAAAAABsY/AeFQzWoyEToQ0wOxTzGQekYnKL5IGt5HQCLcBGAs/s1600/bintang168.png" alt="Bintang168" title="Bintang168" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/--wvmiI_MLMM/XNHYGZsVtAI/AAAAAAAABsc/StSDRpsJ60gO9FNpME7LK-WZ5R99A4ubwCLcBGAs/s1600/brotogel.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-2iLtQl2hCgk/XNHYG9UZCPI/AAAAAAAABsg/GFpuwMm1Fn0QSWAaSDdiTmHeLRxJ6huhgCLcBGAs/s1600/cmcbet.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-UkveXq-8WmQ/XNHYBlHNUCI/AAAAAAAABrc/bCUe4Gu4isQxoXx1mH9lcgAtjVvWezm-wCLcBGAs/s1600/Dewa%2Btogel.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-OLCjsbOeg38/XNHYHNSzkvI/AAAAAAAABsk/ZiiU3bEcUVI3ivPFEWENiGZ77hK0V2WrwCLcBGAs/s1600/dragontoto.gif" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-Nbq0pL3rbz4/XNHYCT1oMBI/AAAAAAAABrk/4kBRgLIpmRMxDqPAdVMnLReNVaRyMQN_wCLcBGAs/s1600/Eyangtogel.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-bLwlrrzab1o/XNHYHQt3EnI/AAAAAAAABso/AGUa-lvQS0oN-OgeCD4bQomL9e59tTZCwCLcBGAs/s1600/galaxytoto.jpg" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-9PuO5ZEfHvo/XNHYHyxgJfI/AAAAAAAABsw/crmwIpk7s7kEquDkpVjRja0rpXs3J8ZcgCLcBGAs/s1600/indotogelnet.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-2ydeb4Etofs/XNHYIrUcw-I/AAAAAAAABs4/919LhMZ41wQGJ9kD3wGKP05zfD_ipLyTACLcBGAs/s1600/indowlatoto.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-kuDH1Kx3H8o/XNHYCzuYYgI/AAAAAAAABro/DkUoG_mjXDAL3FcZEZTkkNmTvR3CaEjVwCLcBGAs/s1600/JAYATOGEL.gif" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-uyS1BMornQo/XNHYIXxYcwI/AAAAAAAABs0/UbqcJs8NF5Q78XAbx6SeGR3Mm9Hp7ByFACLcBGAs/s1600/jp2d.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-1M1H7M-N_qc/XNHYDOTgVRI/AAAAAAAABrs/5noz2ygHu4owVyY1dNWTtRxCV9EwFt7QQCLcBGAs/s1600/King4d.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-kd3_8ZYOwgM/XNHYIkjW33I/AAAAAAAABs8/rD_nDjuKYc4hKJS0-ZQ-er0yiWG72cP8wCLcBGAs/s1600/koperuang.jpg" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-TkZcT-knCd8/XNHYJCjDE-I/AAAAAAAABtE/GUuQsEQaS7kbLb3gbXbedFaA9HZ0_heLQCLcBGAs/s1600/kotauang.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-2ipVD3fCUAA/XNHYDb6rjPI/AAAAAAAABrw/IPOZypmcGcYQUZsUUzAHrht6vQTlZ2T7gCLcBGAs/s1600/Lotto01.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-uJa7mjjSUsE/XNHYJIF6whI/AAAAAAAABtA/O1xYIE8MFVcHgxqvoHHe8KMZ5zQi9XjHwCLcBGAs/s1600/mcity%2Btoto.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-BP3CIefWHnI/XNHYJq8Cf8I/AAAAAAAABtI/4FvxCP4qxs8ETtPXc4G_DhSIR7EFexU0gCLcBGAs/s1600/pulaudewa.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-P0p9tOnrsM0/XNHYJyXt5WI/AAAAAAAABtM/VHvQKGPeP3M9HDq4qWtRGm66iEiBKuEyACLcBGAs/s1600/rgotogel.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-n-uU7F9ZG5Q/XNHYKDJRTrI/AAAAAAAABtQ/RjfzqQHT2dUMn-vqGzUiFcvHMDv3NYfIACLcBGAs/s1600/selebtoto.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-bruKUsh-GcM/XNHYKJjVlOI/AAAAAAAABtU/QIZpIwYlDws-aDTp7vrUCTQ4v3qGsDIPQCLcBGAs/s1600/silva4d.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-p1g13p8-x8M/XNHYKwshYXI/AAAAAAAABtY/xZKfbtaQhFo0W3BQVPjnVMHBxoiOA_vfACLcBGAs/s1600/sinar88.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-FR4OPmMW0WQ/XNHYK8CanHI/AAAAAAAABtc/mJXijuENiO4L0-6M8Sc7J6SmuEu4tEVyQCLcBGAs/s1600/togel4D.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-7SHOTOFWd6M/XNHYLLjky4I/AAAAAAAABtg/J7Xsiqd7Fi8_JZ-Fx_SPbRsJ8RyP6zS8gCLcBGAs/s1600/togelbetnet.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-W8MuPw6HCOI/XNHYL3CZXuI/AAAAAAAABtk/2meUnsFb8IIZLCpz71p_R6RWWDLN6ANyQCLcBGAs/s1600/togelcc.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-gQ25LrLAhwI/XNHYDqQscSI/AAAAAAAABr0/7Lb6a5j_a_c19wio9kWpseIMMeMy__u-QCLcBGAs/s1600/TogelKlub.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-JGb2RZQYtlk/XNHYD59clnI/AAAAAAAABr4/PXOGcT1TWcM4FskfK8sIe93MSLSy3wC9QCLcBGAs/s1600/Togellounge99.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-_H9nslKS42g/XNHYEZ9zYeI/AAAAAAAABr8/TXKPn25RNLAwmYmb3cJ5_dy2neYFt30UQCLcBGAs/s1600/Togelplus.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-xfU8TWSwCHE/XNHYMPaKpVI/AAAAAAAABto/Fe8eYzuqHWUEVhjtPz4P-O923orKjqy2gCLcBGAs/s1600/totobetnet.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://4.bp.blogspot.com/-BkJux9XOUi4/XNHYMdYysQI/AAAAAAAABts/sslXyM815OA-8HWY1EH9fBAedEmLMKvtgCLcBGAs/s1600/totojitu.gif" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://2.bp.blogspot.com/-b3XzHkrXHN8/XNHYMvwchUI/AAAAAAAABtw/o-1HbMPpcr0yhtfoDFUwXJg5JjnUWoyXwCLcBGAs/s1600/totost.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://3.bp.blogspot.com/-csNRZQfVuos/XNHYMz12nhI/AAAAAAAABt0/-v3nNgLmvZUsQqZPJBgwk7NHkoR2Sow9QCLcBGAs/s1600/vegas%2Bgaming.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>
+<a href="http://linktoto.info/daftar29" style="text-decoration: none" target="_blank">
+<img src="https://1.bp.blogspot.com/-IyutRqxaQvY/XNHYNV_0kbI/AAAAAAAABt4/QYp7wPD5SPINf0C5WMBJh4wzRvq-1WVNACLcBGAs/s1600/vegastogel.png" alt="Brotogel" title="Brotogel" style="width:140px;height:40px;border:0" /></a>')
